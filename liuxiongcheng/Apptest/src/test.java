2098a07e406fc18e8d94127c9905b121b4e19adc
liuxiongcheng <1176811062@qq.com>
2016-08-27 00:10:04 +0800

    第二行开始不做任何修改

--- liuxiongcheng/Apptest/src/test.java
+++ liuxiongcheng/Apptest/src/test.java
@@ -1,1 +1,21 @@
 import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lenovo on 2016/8/13.
+ */
+public class test
+{
+    public static void main(String[] args) {
+        Appmain a = new Appmain();
+        a.setX(5);a.setY(10);
+        Appmain b=new Appmain();
+        b.setX(5);b.setY(10);
+        System.out.println(a.getX());
+        List<Appmain>list=new ArrayList<>();
+        list.add(a);
+        list.add(b);
+        System.out.print(list.get(0).getX());
+        System.out.print(list.get(1).getY());
+    }
+}
